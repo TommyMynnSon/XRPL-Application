99a01ee94fafa703e40d113b69af13c69e0e81ca
TommyMynnSon <tommy.m.son@gmail.com>
2021-12-21 18:28:05 -0800

    Install dotenv dependency and modularize wallet generation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 // [ Dependencies ]
+const dotenv = require('dotenv');
+dotenv.config({ path: '../.env' });
+
 const xrpl = require('xrpl');
 
 // [ Variables ]
@@ -10,19 +13,75 @@ const xrpl = require('xrpl');
 // - Notes: Testnet public server
 const PUBLIC_SERVER = 'wss://s.altnet.rippletest.net/';
 
+// Function that showcase various ways of generating/accessing wallets.
+const generateWallet = async (client) => {
+    // ------------ A) Create a wallet and fund it with the Testnet faucet ------------
+    
+    // const fund_result = await client.fundWallet();
+    // const test_wallet = fund_result.wallet;
+
+    // // Expected output of fund_result:
+    // // {
+    // //     wallet: Wallet {
+    // //         publicKey: '...',
+    // //         privateKey: '...',
+    // //         classicAddress: '...',
+    // //         seed: '...'
+    // //     },
+    // //
+    // //     balance: ###
+    // // }
+    // console.log(fund_result);
+
+    // // Expected output of test_wallet
+    // // Wallet {
+    // //     publicKey: '',
+    // //     privateKey: '',
+    // //     classicAddress: '',
+    // //     seed: ''
+    // // }
+    // console.log(test_wallet);
+
+    // return test_wallet;
+
+    // ------------ A) Create a wallet and fund it with the Testnet faucet --------------
+
+    // ------------ B) Create a wallet and just generate its keys -----------------------
+
+    // const test_wallet = xrpl.Wallet.generate();
+
+    // // Expected output of test_wallet
+    // // Wallet {
+    // //     publicKey: '',
+    // //     privateKey: '',
+    // //     classicAddress: '',
+    // //     seed: ''
+    // // }
+    // console.log(test_wallet);
+
+    // ------------ B) Create a wallet and just generate its keys -----------------------
+
+    // ------------ C) Instantiate a wallet from a seed encoded in base58 ---------------
+
+    // ------------ C) Instantiate a wallet from a seed encoded in base58 ---------------
+};
+
+
 // Wrap code in an async function so we can use await
 const main = async () => {
+    console.log(`${process.env.GREETING}`);
+
     // Define the network client
     const client = new xrpl.Client(PUBLIC_SERVER);
 
     // Connect to network client
     await client.connect();
 
-    // Everything I need to do when connected to the network client
-    console.log(client);
+    // Generate wallet
+    const wallet = await generateWallet(client);
 
     // Disconnect from network client when done.
     client.disconnect();
-}
+};
 
 main();
