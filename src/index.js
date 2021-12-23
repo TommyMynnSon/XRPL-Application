61fa8ef43f5d536354ce7d339785174a454683a1
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-12-23 12:53:06 -0800

    Finalize wallet generation, queries to the XRP Ledger, handlers for events on the XRP Ledger, and sending of XRP between wallets

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const xrpl = require('xrpl');
 // - Notes: Testnet public server
 const PUBLIC_SERVER = 'wss://s.altnet.rippletest.net/';
 
-// Function that showcase various ways of generating/accessing wallets.
+// Function that showcases various ways of generating/accessing wallets
 const generateWallet = async (client) => {
     // ------------ A) Create a wallet and fund it with the Testnet faucet ------------
 
@@ -35,10 +35,10 @@ const generateWallet = async (client) => {
 
     // // Expected output of test_wallet
     // // Wallet {
-    // //     publicKey: '',
-    // //     privateKey: '',
-    // //     classicAddress: '',
-    // //     seed: ''
+    // //     publicKey: '...',
+    // //     privateKey: '...',
+    // //     classicAddress: '...',
+    // //     seed: '...'
     // // }
     // console.log(test_wallet);
 
@@ -52,32 +52,134 @@ const generateWallet = async (client) => {
 
     // // Expected output of test_wallet
     // // Wallet {
-    // //     publicKey: '',
-    // //     privateKey: '',
-    // //     classicAddress: '',
-    // //     seed: ''
+    // //     publicKey: '...',
+    // //     privateKey: '...',
+    // //     classicAddress: '...',
+    // //     seed: '...'
     // // }
     // console.log(test_wallet);
+
+    // return test_wallet;
 
     // ------------ B) Create a wallet and just generate its keys -----------------------
 
     // ------------ C) Instantiate a wallet from a seed encoded in base58 ---------------
 
-    // const test_wallet = xrpl.Wallet.fromSeed(process.env.W1_seed);
+    const test_wallet = xrpl.Wallet.fromSeed(process.env.W1_seed);
 
-    // // Expected output of test_wallet (check .env file for W1 secrets)
-    // // Wallet {
-    // //     publicKey: '',
-    // //     privateKey: '',
-    // //     classicAddress: '',
-    // //     seed: ''
-    // // }
-    // console.log(test_wallet);
+    // Expected output of test_wallet
+    // Wallet {
+    //     publicKey: '...',
+    //    privateKey: '...',
+    //    classicAddress: '...',
+    //    seed: '...'
+    // }
+    console.log(test_wallet);
+
+    return test_wallet;
 
     // ------------ C) Instantiate a wallet from a seed encoded in base58 ---------------
 };
 
-// Wrap code in an async function so we can use await
+// Function that showcases various ways to query the XRP Ledger's WebSocket API (more in https://xrpl.org/request-formatting.html)
+const generateQuery = async (client, wallet) => {
+    // const response = await client.request({
+    //     'command': 'account_info',
+    //     'account': wallet.address,
+    //     'ledger_index': 'validated'
+    // });
+
+    // // Expected output of response
+    // // {
+    // //     id: ###,
+    // //     result: {
+    // //       account_data: {
+    // //         Account: '...',
+    // //         Balance: '...',
+    // //         Flags: ###,
+    // //         LedgerEntryType: '...',
+    // //         OwnerCount: ###,
+    // //         PreviousTxnID: '...',
+    // //         PreviousTxnLgrSeq: ###,
+    // //         Sequence: ###,
+    // //         index: '...'
+    // //       },
+    // //       ledger_hash: '...',
+    // //       ledger_index: ###,
+    // //       validated: boolean
+    // //     },
+    // //     type: '...'
+    // // }
+    // console.log(response);
+
+    // return response;
+};
+
+// Function that showcases various ways to add handlers for events from the XRP Ledger
+const generateHandler = (client) => {
+    // // Listen to ledger close events (whenever the XRP Ledger's consensus process produces a new ledger version)
+    // client.request({
+    //     'command': 'subscribe',
+    //     'streams': ['ledger']
+    // });
+
+    // client.on('ledgerClosed', async (ledger) => {
+    //     console.log(`Ledger #${ledger.ledger_index} validated with ${ledger.txn_count} transactions!`)
+    // });
+};
+
+// Function that showcases various ways to send XRP from one wallet to another
+const sendXRP = async (client, sender, receiver) => {
+    // // Prepare transaction
+    // const prepared = await client.autofill({            // <--- autofill() method automatically fills in good defaults for the remaining fields of a transaction
+    //     'TransactionType': 'Payment',
+    //     'Account': sender.address,
+    //     'Amount': xrpl.xrpToDrops('25'),
+    //     'Destination': `${receiver}`
+    // });
+
+    // const max_ledger = prepared.LastLedgerSequence;    // <--- optional LastLedgerSequence is strongly recommended
+
+    // // Expected output of prepared
+    // // {
+    // //     TransactionType: 'Payment',
+    // //     Account: '...',
+    // //     Amount: '100000000',
+    // //     Destination: '...',
+    // //     Flags: 0,
+    // //     Sequence: 23768460,
+    // //     Fee: '6054',
+    // //     LastLedgerSequence: 23818084
+    // // }
+    // console.log('Prepared transaction instructions:', prepared);
+
+    // // Expected output of xrpl.dropsToXrp(prepared.Fee)
+    // // 0.006054
+    // console.log('Transaction cost:', xrpl.dropsToXrp(prepared.Fee), 'XRP');
+
+    // // Expected output of max_ledger
+    // // 23818084
+    // console.log('Transaction expires after ledger:', max_ledger);
+
+    // // Sign the transaction prepared above to authorize the transaction
+    // const signed = sender.sign(prepared);
+
+    // // Outputs the transaction's ID or identifying hash, which can be used to look up the transaction later
+    // // (unique 64 character hexadecimal string)
+    // console.log("Identifying hash:", signed.hash);
+
+    // // Signature represented by the hexadecimal representation of its canonical binary format called a 'blob'
+    // console.log("Signed blob:", signed.tx_blob);
+
+    // // Submit the signed blob to XRP Ledger server
+    // // submitAndWait() submits a signed transaction to the network and waits for the response
+    // // submitSigned() submits a transaction and gets only the preliminary response
+    // const tx = await client.submitAndWait(signed.tx_blob);
+
+    // console.log("Transaction result:", tx.result.meta.TransactionResult);
+    // console.log("Balance changes:", JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2));
+};
+
 const main = async () => {
     // Define the network client
     const client = new xrpl.Client(PUBLIC_SERVER);
@@ -88,8 +190,17 @@ const main = async () => {
     // Generate wallet
     const wallet = await generateWallet(client);
 
+    // Query XRP Ledger's WebSocket API
+    const response = await generateQuery(client, wallet);
+
+    // Set up handlers for events
+    generateHandler(client);
+
+    // Send XRP
+    await sendXRP(client, wallet, process.env.W2_classicAddress);
+
     // Disconnect from network client when done.
-    client.disconnect();
+    // client.disconnect();
 };
 
 main();
